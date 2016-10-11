21e03c23c66ea53494b838c3ed06929e44ec6b01
Sarah Penney <sarahpenneyuk@gmail.com>
2016-10-12 00:30:14 +0100

    and another

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
 <?php include_once 'index.html' ?>
-<?php include_once 'virus.html' ?>
-<?php include_once '2ndLife.html' ?>
-<?php include_once 'guruMountain.html' ?>
-<?php include_once 'snakeGuruLair.html' ?>
-<?php include_once 'BearSnake.html' ?>
